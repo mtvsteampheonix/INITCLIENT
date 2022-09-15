6a52441e6a99cd4aba069f8f0d6e1516373f534c
5onchangwoo <scw9410@gmail.com>
2022-09-15 20:42:15 +0900

    Design: "Footer" 컴포넌트 디자인 변경
    "Footer" 컴포넌트 디자인 변경

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,13 +6,10 @@ const StyledFooter = styled.footer`
   display: flex;
   flex-direction: row;
   justify-content: center;
-  align-items: flex-start;
-  padding-top: 8vmax;
-  padding-bottom: 10vmax;
-  gap: 150px;
+  align-items: center;
   background: #4199e1;
-  width: 100%;
-  height: 10vmax;
+  height: 400px;
+  min-width: 1200px;
 `;
 
 const StyledDetails = styled.div`
@@ -23,44 +20,53 @@ const StyledDetails = styled.div`
   font-size: 1em;
   overflow: hidden;
   nowrap: none;
+  width: 400px;
 `;
 
 const StyledA = styled.a`
-  color: #f1db15; ;
+  color: #f1db15;
+`;
+
+const StyledCSDiv = styled.div`
+  display: flex;
+  width: 1200px;
+  justify-content: space-around;
 `;
 
 function Footer() {
   return (
     <StyledFooter>
-      <StyledDetails>
-        <h2>고객센터</h2>
-        <br />
-        <p>대표 번호 : 1588-1588</p>
-        <br />
-        <p>운영시간 : 오전 9시 ~ 오후 6시 (주말 및 공휴일 휴무)</p>
-        <br />
-        <p>점심시간 : 오후 11시 30분 ~ 오후 1시</p>
-        <br />
-      </StyledDetails>
-      <StyledDetails>
-        <h2>문의</h2>
-        <br />
-        <p>E-mail : bool_size_oh@metaverse.com</p>
-        <br />
-        <StyledA>자주묻는질문 FAQ</StyledA>
-        <br />
-        <StyledA>문의하기</StyledA>
-        <br />
-      </StyledDetails>
-      <StyledDetails>
-        <h2>개인정보 처리방침</h2>
-        <br />
-        <p>이용약관</p>
-        <br />
-        <p>결제/ 환불에 대한 법률</p>
-        <br />
-      </StyledDetails>
-      {/* <p align='center'>Copyrightⓒ2022 team-phoenix All rights reserved.</p> */}
+      <StyledCSDiv>
+        <StyledDetails>
+          <h2>고객센터</h2>
+          <br />
+          <p>대표 번호 : 1588-1588</p>
+          <br />
+          <p>운영시간 : 오전 9시 ~ 오후 6시 (주말 및 공휴일 휴무)</p>
+          <br />
+          <p>점심시간 : 오후 11시 30분 ~ 오후 1시</p>
+          <br />
+        </StyledDetails>
+        <StyledDetails>
+          <h2>문의</h2>
+          <br />
+          <p>E-mail : bool_size_oh@metaverse.com</p>
+          <br />
+          <StyledA>자주묻는질문 FAQ</StyledA>
+          <br />
+          <StyledA>문의하기</StyledA>
+          <br />
+        </StyledDetails>
+        <StyledDetails>
+          <h2>개인정보 처리방침</h2>
+          <br />
+          <p>이용약관</p>
+          <br />
+          <p>결제/ 환불에 대한 법률</p>
+          <br />
+        </StyledDetails>
+        {/* <p align='center'>Copyrightⓒ2022 team-phoenix All rights reserved.</p> */}
+      </StyledCSDiv>
     </StyledFooter>
   );
 }
