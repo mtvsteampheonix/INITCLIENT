5635eb446a56be52d5cbd232e2d9a52917236447
5onchangwoo <scw9410@gmail.com>
2022-09-17 01:05:25 +0900

    Feat: "theme.js" 추가
    전역 테마 설정을 위한 파일 추가 및 적용

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+import {createTheme} from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#4199e1'
+    },
+    gray: {
+      main: '#A9A9A9',
+      contrastText: '#fff'
+    }
+  }
+});
