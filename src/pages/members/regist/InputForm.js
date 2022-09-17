6216ec5c4b9a36e1ec7e7d4a333adde9fb5f93b9
5onchangwoo <scw9410@gmail.com>
2022-09-17 19:10:05 +0900

    Feat: "회원가입 입력 페이지" 추가
    개인 회원 가입 입력 페이지 이미, 텍스트입력폼 한개 추가

--- src/pages/members/regist/InputForm.js
+++ src/pages/members/regist/InputForm.js
@@ -1,5 +1,26 @@
+import {Typography, Grid, Box, Stack, TextField} from '@mui/material';
 function InputForm() {
-  return <>입력폼을 받을 공간입니다.</>;
+  return (
+    <article>
+      <Typography variant='h4' align='center'>
+        개인 회원 가입
+      </Typography>
+      <Box sx={{display: 'flex', justifyContent: 'center'}}>
+        <img style={{width: '300px'}} src='/titleLogo.svg' alt='titleLogo' />
+      </Box>
+      <Stack
+        justifyContent='center'
+        spacing={1}
+        minWidth={800}
+        margin='auto'
+        paddingBottom={10}
+        width={1200}
+        alignContent='center'
+      >
+        <TextField id='outlined-basic' label='Outlined' variant='outlined' />
+      </Stack>
+    </article>
+  );
 }
 
 export default InputForm;
