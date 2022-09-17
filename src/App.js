bd627c221a4e8ec307c5fc7dbacf7fc9de3c0e95
5onchangwoo <scw9410@gmail.com>
2022-09-17 13:05:08 +0900

    Fix: "App.js Manage경로"수정

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import './App.css';
 import Main from './pages/Main';
 import Layout from './layouts/Layout';
-import {Manage} from './pages/resume/manage';
+import {Manage} from './pages/resume/Manage';
 import {AddStepCategory} from './pages/resume/add-step';
 import Login from './pages/members/Login';
 
