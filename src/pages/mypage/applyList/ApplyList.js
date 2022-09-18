ff44b7c551266385e385ee281da9b1d9cd7a74a4
NJWonE <93060795+NJWonE@users.noreply.github.com>
2022-09-19 00:10:48 +0900

    Feat: 지원 내역 조회 페이지 헤더 추가 제목, 처리현황 select box, 검색 input box, 검색, 삭제 버튼 추가

---
+++ src/pages/mypage/applyList/ApplyList.js
@@ -0,0 +1,12 @@
+import ApplyListPageHeader from '../../../components/applyPage/ApplyListPageHeader';
+
+function ApplyList() {
+  return (
+    <>
+      <ApplyListPageHeader />
+      <h1>어플라이 리스트</h1>
+    </>
+  );
+}
+
+export default ApplyList;
--- src/routes/MyPageRoute.js
+++ src/routes/MyPageRoute.js
@@ -1,10 +1,12 @@
 import {Route, Routes} from 'react-router-dom';
+import ApplyList from '../pages/mypage/applyList/ApplyList';
 import Error404 from './../pages/errors/Error404';
 export default function MyPageRoute() {
   return (
     <Routes>
       {/* //이곳에 페이지 추가해주세요  */}
 
+      <Route path='applylist' element={<ApplyList />} />
       <Route path='*' element={<Error404 />} />
     </Routes>
   );
