3752aa826714e5d6d9b1520d131b4d537d688f54
5onchangwoo <scw9410@gmail.com>
2022-09-19 17:46:47 +0900

    Refactor: "ApplyHistory" 변경
    mypage 구조 변경에 따른 ApplyHistroy 변경

--- src/pages/mypage/member/ApplyHistory.js
+++ src/pages/mypage/member/ApplyHistory.js
@@ -1,3 +1,9 @@
+import ApplyList from './../applyList/ApplyList';
+
 export default function ApplyHistory() {
-  return <>지원 내역 페이지 입니다. 파이팅</>;
+  return (
+    <>
+      <ApplyList />
+    </>
+  );
 }
