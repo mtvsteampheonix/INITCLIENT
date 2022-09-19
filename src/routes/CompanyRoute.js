e199d087e2c6619502e4bca0405c586be833a77a
chlgks2 <duddngud3@gmail.com>
2022-09-19 21:49:48 +0900

    Feat: "기업 목록 조회 페이지" 추가
    페이지 생성 및 컴포넌트 추가

--- src/routes/CompanyRoute.js
+++ src/routes/CompanyRoute.js
@@ -1,9 +1,12 @@
 import {Route, Routes} from 'react-router-dom';
 import Error404 from '../pages/errors/Error404';
+import Company from '../pages/company/Company';
+
 export default function CompanyRoute() {
   return (
     <Routes>
       {/* //이곳에 페이지 추가해주세요  */}
+      <Route index element={<Company />} />
 
       <Route path='*' element={<Error404 />} />
     </Routes>
