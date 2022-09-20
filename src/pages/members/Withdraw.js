8033b834b71f642ac8cbbbeeba835dbe686cbc16
5onchangwoo <scw9410@gmail.com>
2022-09-20 17:12:27 +0900

    Feat: "회원탈퇴 페이지" 추가
    개인회원, 기업회원이 사용할 회원탈퇴 페이지 추가

---
+++ src/pages/members/Withdraw.js
@@ -0,0 +1,12 @@
+import {Grid, Typography} from '@mui/material';
+export default function Withdraw() {
+  return (
+    <>
+      <Grid container>
+        <Grid item xs={12}>
+          <Typography>INIT 회원 탈퇴</Typography>
+        </Grid>
+      </Grid>
+    </>
+  );
+}
