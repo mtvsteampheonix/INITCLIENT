197bfb4a4d4ca6bc48cca8924c1f210b186d5938
5onchangwoo <scw9410@gmail.com>
2022-09-22 16:46:50 +0900

    Feat: "페이지 전환시 맨위로 가는 기능" 추가
    index.js에 컴포넌트 추가

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import {Provider} from 'react-redux';
 import store from './store';
 import {createTheme, ThemeProvider} from '@mui/material/styles';
 import {theme} from './theme';
+import ScrollToTop from './Scroll-to-top';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
